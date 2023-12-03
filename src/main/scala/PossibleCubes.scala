cce917074f03ea47b7fda7df8a6cb7cf837f6274
Soner Guzeloglu <guzeloglusoner@gmail.com>
2023-12-03 21:31:56 +0100

    Advent of Code day 2 part 2 implemented.

--- src/main/scala/PossibleCubes.scala
+++ src/main/scala/PossibleCubes.scala
@@ -5,16 +5,44 @@ import scala.io.Source
 object PossibleCubes extends App {
 
   private val games = Source.fromFile("src/main/resources/possible_cubes.txt").getLines().toList
-  private val possibleGames = games.filter(isPossible)
+  /*private val possibleGames = games.filter(isPossible)
   private val result: Int = possibleGames
     .map(e => {
       val endIndex = e.indexOf(":")
       val beginningIndex = e.indexOf(" ")
       e.substring(beginningIndex + 1, endIndex).toInt
   })
-    .sum
+    .sum*/
 
-  println(result)
+  private val minimumSetOfCubes = games
+    .map( e => {
+      val semicolonIndex = e.indexOf(":") + 1
+      val setOfCubes: List[String] = e
+        .substring(semicolonIndex)
+        .split(";")
+        .toList
+
+      extractValue(setOfCubes, "red") *
+        extractValue(setOfCubes, "blue") *
+          extractValue(setOfCubes,"green")
+    })
+
+  println(minimumSetOfCubes)
+
+  println(minimumSetOfCubes.sum)
+  private def extractValue(set: List[String], color: String): Int = {
+    val numOfCubes: List[Int] = set
+      .filter(_.contains(color))
+      .map(a => {
+        val pattern = ("(\\d+)(?= " + color + ")").r
+        pattern.findFirstIn(a) match
+          case Some(value) => value.toInt
+          case None => 0
+      })
+
+    numOfCubes.max
+  }
+
   private def isPossible(str: String): Boolean = {
     val semicolonIndex = str.indexOf(":") + 1
     val setOfCubes: List[String] = str
@@ -32,9 +60,9 @@ object PossibleCubes extends App {
     val maxRedCubes = 12
     val maxGreenCubes = 13
     val maxBlueCubes = 14
-    val numOfRedCubes: Int = extractValue(str,"red")
-    val numOfBlueCubes: Int = extractValue(str,"blue")
-    val numOfGreenCubes: Int = extractValue(str,"green")
+    val numOfRedCubes: Int = extractPossibleValue(str,"red")
+    val numOfBlueCubes: Int = extractPossibleValue(str,"blue")
+    val numOfGreenCubes: Int = extractPossibleValue(str,"green")
 
     if(maxRedCubes < numOfRedCubes
       || maxBlueCubes < numOfBlueCubes
@@ -46,7 +74,7 @@ object PossibleCubes extends App {
       true
   }
 
-  private def extractValue(set: String, color: String): Int = {
+  private def extractPossibleValue(set: String, color: String): Int = {
     set
       .split(",")
       .toList
