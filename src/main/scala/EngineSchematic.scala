251452269de0f2cc344acf0dba7302c7aab199a8
Soner Guzeloglu <guzeloglusoner@gmail.com>
2023-12-04 23:26:19 +0100

    Day 3 Part 2 completed.

--- src/main/scala/EngineSchematic.scala
+++ src/main/scala/EngineSchematic.scala
@@ -6,19 +6,65 @@ import scala.util.matching.Regex
 object EngineSchematic extends App {
 
   private case class NumberLocations(number: Int, location: Int, lineNumber: Int)
+
+  private case class GearLocations(location: Int, lineNumber: Int)
+
   private val schematicLines: List[(String, Int)] = Source.fromFile("src/main/resources/engine_schematic.txt").getLines().toList.zipWithIndex
 
-  private val extractedNumberLocations: List[Int] =
-    schematicLines
-      .flatMap(e => {
-      extractNumberLocations(e._1, e._2)
-    })
-      .filter(f => {
-      checkIfNumberEligible(schematicLines, f)
-    })
-      .map(_.number)
+  private val gearLocations: List[GearLocations] = schematicLines
+    .flatMap(e => getGearLocations(e._1, e._2))
 
-  println(extractedNumberLocations.sum)
+  private val eligibleGears: List[List[Int]] = gearLocations
+    .map(b => getListOfGearAdjacentNumbers(schematicLines, b))
+
+  private val filteredGears: List[List[Int]] = eligibleGears.filter(_.size == 2)
+
+  private val multipliedGears: List[Int] = filteredGears.map(gearNumberList => {
+    gearNumberList.head * gearNumberList.last
+  })
+
+  println(multipliedGears.sum)
+
+
+  /*  private val extractedNumberLocations: List[Int] =
+      schematicLines
+        .flatMap(e => {
+        extractNumberLocations(e._1, e._2)
+      })
+        .filter(f => {
+        checkIfNumberEligible(schematicLines, f)
+      })
+        .map(_.number)
+
+
+    println(extractedNumberLocations.sum)*/
+
+  private def getGearLocations(schematicLine: String, lineNumber: Int): List[GearLocations] = {
+    val pattern = "\\*".r
+    val gears = pattern.findAllIn(schematicLine).toList
+    var globalIndex: Int = 0
+    val gearsList: List[GearLocations] = gears.map(e => {
+      val startingIndex = schematicLine.indexOf(e, globalIndex)
+      globalIndex = Math.max(globalIndex, startingIndex) + 1
+      GearLocations(startingIndex, lineNumber)
+    })
+
+    gearsList
+  }
+
+  private def extractNumberLocations(schematicLine: String, lineNumber: Int): List[NumberLocations] = {
+    val pattern = "\\d+".r
+    val numbers = pattern.findAllIn(schematicLine).toList
+    var globalIndex: Int = 0
+    val numLocList = numbers.map(e => {
+      val number = e.toInt
+      val startingIndex = schematicLine.indexOf(e, globalIndex)
+      globalIndex = Math.max(globalIndex, startingIndex) + number.toString.length
+      NumberLocations(number, startingIndex, lineNumber)
+    })
+
+    numLocList
+  }
 
   private def checkIfNumberEligible(schematicLines: List[(String, Int)], number: NumberLocations) = {
     val lineNumber = number.lineNumber
@@ -31,11 +77,59 @@ object EngineSchematic extends App {
     val searchResult = searchSpace.map(e => {
       !checkTheStringRange(e._1, searchRange)
     })
-    println("LINE NUMBER: " +lineNumber + " AND Returned " + searchResult + " for the number: " + number.number)
+    println("LINE NUMBER: " + lineNumber + " AND Returned " + searchResult + " for the number: " + number.number)
     searchResult.exists(_.self == true)
   }
 
-  private def checkTheStringRange(searchStr: String, searchRange:Seq[Int]): Boolean = {
+  private def getListOfGearAdjacentNumbers(schematicLines: List[(String, Int)], gear: GearLocations): List[Int] = {
+    val lineNumber = gear.lineNumber
+    val gearStartIndex = gear.location
+    val gearLengh = 1
+
+    val searchRange = Math.max(0, gearStartIndex - 1) to Math.min(gearStartIndex + gearLengh, schematicLines.head._1.length - 1)
+    val searchSpace: List[(String, Int)] = schematicLines.filter(e => e._2 == lineNumber || e._2 == lineNumber - 1 || e._2 == lineNumber + 1)
+
+    val searchResult: List[Int] = searchSpace.flatMap(e => {
+      getGearNumber(e._1, searchRange)
+    }).filter(_ != 0)
+
+    searchResult
+  }
+
+  private def getGearNumber(searchStr: String, searchRange: Seq[Int]): Seq[Int] = {
+    var counter = 0
+    searchRange.map(index => {
+      val isPrevCandidateCharDigit = searchStr.charAt(index - 1).isDigit
+      val candidateChar = searchStr.charAt(index)
+      val digitEquality = candidateChar.isDigit
+      if (digitEquality) {
+        val num = getNumber(searchStr, index)
+        if (counter != 0 && isPrevCandidateCharDigit) {
+          counter += 1
+          0
+        } else {
+          counter +=1
+          num
+        }
+      } else {
+        0
+      }
+    })
+  }
+
+  private def getNumber(str: String, strIndex: Int) = {
+    val pattern = "\\d+".r
+
+    val lastChar = if(str.take(strIndex).last.equals('*')) "*" else "."
+
+    val candidateRange = str.substring(Math.max(0, str.take(strIndex).lastIndexOf(lastChar) + 1))
+    val number = pattern.findFirstIn(candidateRange).get.toInt
+
+    number
+
+  }
+
+  private def checkTheStringRange(searchStr: String, searchRange: Seq[Int]): Boolean = {
     val resultCalc = searchRange.forall(index => {
       val candidateChar: Char = searchStr.charAt(index)
       val dotEquality = candidateChar.compare('.') == 0
@@ -46,17 +140,5 @@ object EngineSchematic extends App {
 
     resultCalc
   }
-  private def extractNumberLocations(schematicLine: String, lineNumber: Int): List[NumberLocations] = {
-    val pattern = "\\d+".r
-    val numbers = pattern.findAllIn(schematicLine).toList
-    var globalIndex: Int = 0
-    val numLocList = numbers.map(e => {
-      val number = e.toInt
-      val startingIndex = schematicLine.indexOf(e, globalIndex)
-      globalIndex = Math.max(globalIndex, startingIndex ) + number.toString.length
-      NumberLocations(number, startingIndex , lineNumber)
-    })
 
-    numLocList
-  }
 }
